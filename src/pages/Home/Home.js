75f74e79493f817984430bda409eaf40d972d5ff
Diego <diegovillarreal123@gmail.com>
2021-09-10 09:43:02 -0500

    changed html route

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,28 +1,25 @@
 import React from "react";
 import "./style.css";
-import diego from "../../images/diego.jpg"
+import diego from "../../images/diego.jpg";
 
 function Home() {
   return (
-      <div className="header">
-        <div className="grid-container">
-         <h1 className="name">Hi, my name is Diego</h1>
-          <p className="description">
-            I'm a Full Stack Web Developer from Chicago, IL.
-          </p>
-            <div className="section">
-              <img
-                src={diego}
-                className="diego"
-              />
-        </div>
-        
+    <div className="header">
+      <div className="grid-container">
+        <h1 className="name">Hi, my name is Diego</h1>
+        <p className="description">
+          I'm a Full Stack Web Developer from Chicago, IL.
+        </p>
+        <div className="section">
+          <img
+            src={diego}
+            className="diego"
+            alt="men with blue sweater wearing glasses and full beard"
+          />
         </div>
       </div>
+    </div>
   );
 }
 
-
 export default Home;
-
-
