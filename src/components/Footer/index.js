281860fa0b8fd10c89e8b24e92f1ff4d2b651b36
Diego <diegovillarreal123@gmail.com>
2021-09-10 09:51:05 -0500

    added description to blank target

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,30 +3,31 @@ import "./style.css";
 
 function Footer() {
   return (
-    <div className="jumbotron">
-
-    <ul class="nav justify-content-center fixed-bottom">
-      <li class="nav-item">
-        <a
-          style={{ display: "table-cell" }}
+    <div className="footer">
+      <ul class="nav justify-content-center fixed-bottom">
+        <li class="nav-item">
+          <a
+            style={{ display: "table-cell" }}
             href="https://github.com/diego773"
-            target="_blank"
-            class="nav-link">
-          <i class="fab fa-github"></i>
-        </a>
-      </li>
-      <li class="nav-item">
-        <a
-          style={{display: "table-cell"}}
-          href="https://www.linkedin.com/in/diego-villarreal-6771a4129/"
-          target="_blank"
-          class="nav-link">
-          <i class="fab fa-linkedin-in"></i>
-        </a>
-      </li>
-    </ul>
+            target="_blank github"
+            class="nav-link"
+          >
+            <i class="fab fa-github"></i>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a
+            style={{ display: "table-cell" }}
+            href="https://www.linkedin.com/in/diego-villarreal-6771a4129/"
+            target="_blank linkedin"
+            class="nav-link"
+          >
+            <i class="fab fa-linkedin-in"></i>
+          </a>
+        </li>
+      </ul>
     </div>
-  )
+  );
 }
 
 export default Footer;
