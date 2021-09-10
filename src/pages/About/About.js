b912f67eca252527a867aa2500a166082652c12d
Diego <diegovillarreal123@gmail.com>
2021-09-10 09:53:23 -0500

    added alt image

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,21 +1,23 @@
 import React from "react";
-import "./style.css"
-import diego from "../../images/diego.jpg"
+import "./style.css";
+import diego from "../../images/diego.jpg";
 
 function About() {
   return (
-    <div className="section" >
+    <div className="section">
       <div className="grid-container">
         <h1 className="about">About Me!</h1>
         <p className="about-me">
-          My name is Diego Villarreal. I'm a Full Stack Web Developer graduate from Northwestern University Coding Bootcamp in Evanston, IL.
-          I have front-end experience with HTML, CSS, JavaScript and React.js. I also have back-end
-          experience in Node.js, MoongoseDB.
+          My name is Diego Villarreal. I'm a Full Stack Web Developer graduate
+          from Northwestern University Coding Bootcamp in Evanston, IL. I have
+          front-end experience with HTML, CSS, JavaScript and React.js. I also
+          have back-end experience in Node.js, MoongoseDB.
         </p>
         <div className="section">
           <img
             src={diego}
             className="diego"
+            alt="men with glasses and full beard wearing blue sweater"
           />
         </div>
       </div>
@@ -24,5 +26,3 @@ function About() {
 }
 
 export default About;
-      
-
